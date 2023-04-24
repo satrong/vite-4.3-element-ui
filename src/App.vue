ada133ac8f9f7cd02a5a220262bd00edef73676d
caohuichang <satrong@foxmail.com>
2023-04-24 02:02:59 +0000

    add files

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const list = [
+  { name: 'a', age: 1 },
+  { name: 'b', age: 2 },
+  { name: 'c', age: 3 },
+]
+</script>
+
+<template>
+<div>
+  <div>
+    <ElButton @click="count++">click</ElButton> <span>{{ count }}</span>
+  </div>
+  <ElTable :data="list">
+    <ElTableColumn label="Name" prop="name"></ElTableColumn>
+    <ElTableColumn label="Age" prop="age"></ElTableColumn>
+  </ElTable>
+</div>
+</template>
---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue2'
+
+export default defineConfig({
+  plugins: [Vue()]
+})
